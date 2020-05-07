29f66c935e7a824f7c3e16a2b9ba5e43160a8da8
Agata Pacholczyk <agataa.pacholczyk@gmail.com>
2020-05-07 17:09:51 +0200

    add BaseWidget

---
+++ src/js/components/_BaseWidget.js
@@ -0,0 +1,70 @@
+class BaseWidget{
+  constructor(wrapperElement, initialValue){
+    const thisWidget = this; 
+
+    thisWidget.dom = {};
+    thisWidget.dom.wrapper = wrapperElement;
+
+    thisWidget.correctValue = initialValue;
+
+  }
+
+  get value(){
+    const thisWidget = this;
+    return thisWidget.correctValue;
+
+  }
+
+  set value(value){
+    const thisWidget = this;
+    const newValue = this.parseValue(value);
+  
+    /*TODO: Add Validation*/
+      
+    if(newValue != thisWidget.correctValue && thisWidget.isValid(newValue)){
+      thisWidget.correctValue = newValue;
+
+      setTimeout(() => {
+        thisWidget.announce();
+      }, 0);
+    }
+
+    thisWidget.renderValue();
+  }
+  setValue(value){
+    const thisWidget = this;
+
+    thisWidget.value = value;
+
+  }
+
+  parseValue(value){
+    return parseInt(value);
+  }
+
+  isValid(value){
+    return !isNaN(value);
+
+  }
+
+  renderValue(){
+    const thisWidget = this;
+
+    thisWidget.dom.input.innerHTML = thisWidget.value;
+  }
+
+  announce(){
+    const thisWidget = this;
+
+    //const event = new Event('updated');
+      
+    const event = new CustomEvent('updated', {
+      bubbles: true
+    });
+      
+    thisWidget.dom.wrapper.dispatchEvent(event);
+  }
+
+}
+
+export default BaseWidget; 
